7515f9f6702549ab8babb762cf9bc04fe92c7562
AJ <63066039+ajworks13@users.noreply.github.com>
2022-09-05 15:07:48 -0400

    Update Main.java
    Prints in one line and at random prints enough to make a sentence and then occasionally also break it into a paragraph.

--- Main.java
+++ Main.java
@@ -7,16 +7,21 @@ class Main {
 		String[] matrixPrint = { "A", "B", "C", "D", "E", "1", "2", "3", "4",
 				"5", "6", "7", "8", "9", "!", "@", "#", "$",
 				"%", "^", "&" };
-		int count = 0;
 
 		do {
 			
 			int randomNumber = rand.nextInt(21);
-			Thread.sleep(100);
-			count+=1;
+			int randomSentenceBreak = rand.nextInt(50);
+			int lineBreaker = rand.nextInt(200);
+			Thread.sleep(50);
 			
-			if(count == 15){
-				System.out.println("-----");
+			if(randomSentenceBreak == 15){
+				System.out.println("");
+			}
+
+			if(lineBreaker == 100){
+				System.out.println("");
+				System.out.println("");
 			}
 
 			switch (randomNumber) {
@@ -36,55 +41,55 @@ class Main {
 					System.out.print(matrixPrint[4]);
 					break;
 				case 5:
-					System.out.println(matrixPrint[5]);
+					System.out.print(matrixPrint[5]);
 					break;
 				case 6:
-					System.out.println(matrixPrint[6]);
+					System.out.print(matrixPrint[6]);
 					break;
 				case 7:
-					System.out.println(matrixPrint[7]);
+					System.out.print(matrixPrint[7]);
 					break;
 				case 8:
-					System.out.println(matrixPrint[8]);
+					System.out.print(matrixPrint[8]);
 					break;
 				case 9:
-					System.out.println(matrixPrint[9]);
+					System.out.print(matrixPrint[9]);
 					break;
 				case 10:
-					System.out.println(matrixPrint[10]);
+					System.out.print(matrixPrint[10]);
 					break;
 				case 11:
 					System.out.print(matrixPrint[11]);
 					break;
 				case 12:
-					System.out.println(matrixPrint[12]);
+					System.out.print(matrixPrint[12]);
 					break;
 				case 13:
-					System.out.println(matrixPrint[13]);
+					System.out.print(matrixPrint[13]);
 					break;
 				case 14:
-					System.out.println(matrixPrint[14]);
+					System.out.print(matrixPrint[14]);
 					break;
 				case 15:
 					System.out.print(matrixPrint[15]);
 					break;
 				case 16:
-					System.out.println(matrixPrint[16]);
+					System.out.print(matrixPrint[16]);
 					break;
 				case 17:
-					System.out.println(matrixPrint[17]);
+					System.out.print(matrixPrint[17]);
 					break;
 				case 18:
 					System.out.print(matrixPrint[18]);
 					break;
 				case 19:
-					System.out.println(matrixPrint[19]);
+					System.out.print(matrixPrint[19]);
 					break;
 				case 20:
-					System.out.println(matrixPrint[20]);
+					System.out.print(matrixPrint[20]);
 					break;
 				case 21:
-					System.out.println(matrixPrint[21]);
+					System.out.print(matrixPrint[21]);
 					break;
 			}
 			
